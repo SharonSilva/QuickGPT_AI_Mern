dfae5177e1688e1350499e100879541c8a22832c
Andrea Sharon Silva <sharonandrea245@gmail.com>
2026-02-07 08:32:29 +0530

    Mounted pages Chat, Credits and Community in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Sidebar from './components/Sidebar'
+import {Route, Routes} from 'react-router-dom'
+import Credits from './pages/Credits'
 
 const App = () => {
   return (
+    <>
     <div>
+      <Sidebar/>
+      <Routes>
+        <Route path='/' element={<ChatBox/>}/>
+        <Route path='/credits' element={Credits}/>
+        <Route path='/comunity' element={<Community/>}/>
+      </Routes>
+      </div>
       
-    </div>
+    </>
   )
 }
 
